7ef0d044525f3e2574c6f32f642d540620763115
leomarkpaway <leomarkpaway@gmail.com>
2024-07-08 19:58:26 +0800

    :sparkles: Setup navigation controller

--- app/src/main/java/com/leomarkpaway/navigationcomponentandbottomnavigation/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/navigationcomponentandbottomnavigation/MainActivity.kt
@@ -2,10 +2,19 @@ package com.leomarkpaway.navigationcomponentandbottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navButtons = findViewById<BottomNavigationView>(R.id.nav_buttons)
+        val navController = findNavController(R.id.nav_host)
+        navButtons.setupWithNavController(navController)
+        
     }
 }
